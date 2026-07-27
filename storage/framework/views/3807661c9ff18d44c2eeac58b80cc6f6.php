050d829cbc4ae6a7c0cadaed6bde8e2c4e8556e8
Fayeadele <sigata452@gmail.com>
2026-07-27 12:04:06 +0000

    initialisation partie front

---
+++ storage/framework/views/3807661c9ff18d44c2eeac58b80cc6f6.php
@@ -0,0 +1,57 @@
+<?php $__env->startPush('styles'); ?>
+  <link rel="stylesheet" href="<?php echo e(asset('assets/css/app.css')); ?>">
+<?php $__env->stopPush(); ?>
+
+<?php $__env->startSection('body_attributes'); ?>
+class="app-body" <?php echo $__env->yieldContent('page_attributes'); ?>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('body'); ?>
+  <?php if (isset($component)) { $__componentOriginal2880b66d47486b4bfeaf519598a469d6 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal2880b66d47486b4bfeaf519598a469d6 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.sidebar','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('sidebar'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal2880b66d47486b4bfeaf519598a469d6)): ?>
+<?php $attributes = $__attributesOriginal2880b66d47486b4bfeaf519598a469d6; ?>
+<?php unset($__attributesOriginal2880b66d47486b4bfeaf519598a469d6); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal2880b66d47486b4bfeaf519598a469d6)): ?>
+<?php $component = $__componentOriginal2880b66d47486b4bfeaf519598a469d6; ?>
+<?php unset($__componentOriginal2880b66d47486b4bfeaf519598a469d6); ?>
+<?php endif; ?>
+  <?php if (isset($component)) { $__componentOriginal5b09c79149dfb771c232996af5f9dae4 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal5b09c79149dfb771c232996af5f9dae4 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.flash-messages','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('flash-messages'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal5b09c79149dfb771c232996af5f9dae4)): ?>
+<?php $attributes = $__attributesOriginal5b09c79149dfb771c232996af5f9dae4; ?>
+<?php unset($__attributesOriginal5b09c79149dfb771c232996af5f9dae4); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal5b09c79149dfb771c232996af5f9dae4)): ?>
+<?php $component = $__componentOriginal5b09c79149dfb771c232996af5f9dae4; ?>
+<?php unset($__componentOriginal5b09c79149dfb771c232996af5f9dae4); ?>
+<?php endif; ?>
+
+  <?php echo $__env->yieldContent('content'); ?>
+
+  <div class="mobile-overlay" data-sidebar-close aria-hidden="true"></div>
+<?php $__env->stopSection(); ?>
+
+
+<?php echo $__env->make('layouts.base', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\Baye Saliou DIAW\Downloads\SICORE-Frontend-Laravel12-Mode-Test-Complet\sicore-front-laravel-mode-test\resources\views/layouts/app.blade.php ENDPATH**/ ?>
