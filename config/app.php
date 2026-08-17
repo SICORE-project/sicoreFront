104ce93281da284eeeeadb70c9ad01d29b64b283
Awa <hadiatoudiawdiop@gmail.com>
2026-08-17 20:45:52 +0000

    Testintegrationeviche

--- config/app.php
+++ config/app.php
@@ -132,6 +132,6 @@ return [
     |
     */
 
-    'api_url' => env('API_URL', 'http://127.0.0.1:8000/api'),
+    'api_base_url' => env('API_BASE_URL', 'http://127.0.0.1:8000/api'),
 
 ];
--- config/services.php
+++ config/services.php
@@ -36,7 +36,7 @@ return [
     ],
     
     'backend' => [
-        'url' => env('API_BASE_URL'),
-    ],
+    'url' => env('API_BASE_URL', 'http://127.0.0.1:8000/api'),
+],
 
 ];
